840fde7dd23dd4cbf0ffe692a5219da8c1dcda87
SUS_BSalt <shitMEMEME@meaningless>
2023-04-17 22:18:16 +0800

    应该把avg模块搬过去了，等序章的关卡管理版号后开始测试

--- data/objects/AVGOBJ.py
+++ data/objects/AVGOBJ.py
@@ -29,6 +29,8 @@ class AVGModule:
 
         self.characterDict = {}
 
+        self.onStageCharacterList = []
+
         self.book = book
 
         self.textBox = None
@@ -126,7 +128,7 @@ class AVGModule:
             for character in self.characterDict:
                 self.characterDict[character].act()
 
-    def act(self):
+    def update(self):
         """接下来是很长的一串判定树，别急，我会在每一层都进行说明"""
         if self.logsBox.activeSituation == True:
             #logs界面开启时
@@ -159,10 +161,10 @@ class AVGModule:
                     #（常规输出的方法）没有点击，自身活跃，无睡眠开关
                     self.timer += 1
                     if self.timer >= self.printSpeed:
-                        self.reader.act()
-                        for character in self.characterDict:
-                            if self.characterDict[character].activeSituation == True:
-                                self.characterDict[character].currentExpression.mouthAct()
+                        self.reader.update()
+                        for character in self.onStageCharacterList:
+                            if character.activeSituation == True:
+                                character.currentExpression.mouthAct()
                         self.timer = 0
                     #（常规输出的方法）
             else:
@@ -193,33 +195,375 @@ class AVGModule:
                 self.timer += 1
                 if self.timer >= self.printSpeed:
                     self.reader.act()
-                    for character in self.characterDict:
-                        if self.characterDict[character].activeSituation == True:
-                                self.characterDict[character].currentExpression.mouthAct()
+                    for character in self.onStageCharacterList:
+                            if character.activeSituation == True:
+                                character.currentExpression.mouthAct()
                     self.timer = 0
 
             self.autoPlaySwitch = False
             self.clickSwitch = False
             self.sleepSwitch = False
 
-
-                    
-    def draw(self):
-        GE.camera.cameraShot.fill((255,255,255))
         GE.camera.draw(self.textBox.vision, self.textBox.loc)
-        for character in self.characterDict:
-            if self.characterDict[character].onStage == True:
-                self.characterDict[character].currentExpression.vision.blit(self.characterDict[character].currentExpression.mouth.vision, self.characterDict[character].currentExpression.mouth.loc)
-                self.characterDict[character].currentExpression.vision.blit(self.characterDict[character].currentExpression.eyes.vision, self.characterDict[character].currentExpression.eyes.loc)
-                GE.camera.draw(self.characterDict[character].currentExpression.vision, self.characterDict[character].loc)
+        for character in self.onStageCharacterList:
+            character.draw()
         if self.logsBox.activeSituation == True:
             GE.camera.draw(self.logsBox.vision, self.logsBox.loc)
-        
 
         
 
     def animate(self):
-        for character in self.characterDict:
-            self.characterDict[character].animate()            
+        for character in self.onStageCharacterList:
+            if character.activeSituation == True:
+                character.currentExpression.mouthAct()       
         if self.logsBox.activeSituation == True:
             self.logsBox.animate()
+
+class Reader:
+    def __init__(self,AVGModule):
+        
+        self.master = AVGModule
+        self.currentSentence = self.master.book.readline().strip()
+        self.currentWord = 0
+        
+        self.textEffectSwitch = False
+
+        
+    def update(self):
+        try:
+            match self.currentSentence[self.currentWord]:
+                #匹配特殊字符，否则直接输出
+                case "$":
+                    #输出下一个字符
+                    self.currentWord += 1
+                    self.master.textBox.printer(self.currentSentence[self.currentWord])
+                    self.currentWord += 1
+                case "《":
+                    self.textEffectSwitch = True
+                    self.textEffectMatcher()
+                case '[':
+                    self.textEffectSwitch = True
+                    self.identifyCharacters()
+                case _:
+                    self.master.textBox.printer(self.currentSentence[self.currentWord])
+                    self.currentWord += 1
+
+
+        except:
+            self.currentSentence = self.master.book.readline().strip()
+            self.currentWord = 0
+            self.master.textBox.currentWordLoc = [self.master.textBox.size[0] + self.master.textBox.margins,
+                                                   self.master.textBox.size[1] + self.master.textBox.margins]
+            self.master.workingSituation = False
+            self.master.logsBox.textVisionList.insert(0,self.master.textBox.vision)
+            for character in self.master.characterDict:
+                self.master.characterDict[character].init()
+            #读取下一句并初始化
+    
+    def identifyCharacters(self):
+        characterName = ""
+        while self.textEffectSwitch:
+            self.currentWord += 1
+            if self.currentSentence[self.currentWord] == "]":
+                self.currentWord += 1
+                self.textEffectSwitch = False
+                break
+            characterName += self.currentSentence[self.currentWord]
+        #将人物名字读取进来
+
+        self.master.textBox.printer(characterName, self.master.characterDict[characterName].color)
+        self.master.textBox.printer(": ")
+        self.master.characterDict[characterName].activeSituation = True
+
+
+    def textEffectMatcher(self):
+        self.textEffect = ""
+        while self.textEffectSwitch:
+            self.currentWord += 1
+            if self.currentSentence[self.currentWord] == "》":
+                self.currentWord += 1
+                self.textEffectSwitch = False
+                break
+            self.textEffect += self.currentSentence[self.currentWord]
+        #将文本特殊样式读取进来
+
+        match self.textEffect:
+            #颜色
+            case _ if self.textEffect[0] == 'c':
+                if self.textEffect[1:] == 'red':
+                    self.master.textBox.textColor = (255,20,20)
+                else:
+                    self.textEffect = self.textEffect[1:].split(',')
+                    self.master.textBox.textColor = tuple(int(n) for n in self.textEffect)
+            case '/c':
+                self.master.textBox.textColor = setting.textColor
+            #加粗
+            case 'b' :
+
+                self.master.textBox.textSize = setting.textBlodSize
+                self.master.textBox.font = pygame.font.Font(setting.charBlodType,self.master.textBox.textSize)
+                self.master.textBox.currentWordLoc[1] -= 5
+            case '/b':
+                self.master.textBox.textSize = setting.textSize
+                self.master.textBox.font = pygame.font.Font(setting.charType,self.master.textBox.textSize)
+                self.master.textBox.currentWordLoc[1] += 5
+            
+            #停顿
+            case _ if self.textEffect[:4] == 'wait':
+                self.master.sleepSwitch = True
+                self.master.sleepTime = float(self.textEffect[4:])*setting.logicLoopFps
+                
+            #换行
+            case '/n':
+                self.master.textBox.wrapText()
+
+            case _ if self.textEffect[:4] == 'face':
+                self.master.characterDict[self.textEffect[6:]].setExpression(int(self.textEffect[4:6]))
+            case _ if self.textEffect[:6] == 'appear':
+                self.master.characterDict[self.textEffect[6:]].onStage = True
+            case _ if self.textEffect[:5] == 'leave':
+                self.master.characterDict[self.textEffect[5:]].onStage = False
+
+            case 'END':
+                self.master.eventList.append("END")
+            case _ :
+                print("未知效果"+self.textEffect)
+
+class TextBox:
+    """文本框，
+    存了文本大小颜色等设置，以及打印与换行，
+    记得给frameList添加图片，执行初始化方法"""
+    def __init__(self, size, loc):
+        self.size = size
+        self.loc = loc
+        self.rect = self.loc + self.size
+
+        self.vision = None
+        self.frameList = []
+
+        self.textColor = setting.textColor
+        """文字颜色"""
+        self.textSize = setting.textSize
+        """文字大小"""
+        self.textLineGap = setting.textLineGap
+        """#行间距"""
+        self.margins = setting.margins
+        """#页边距"""
+        self.textLineCutLineLoc = self.size[0] - self.margins*2
+        """#换行的边线坐标"""
+
+        self.charType = setting.charType
+        self.font = pygame.font.Font(self.charType,self.textSize)
+        """字体"""
+
+        self.currentWordLoc = [self.margins, self.margins]
+
+        self.textLineCutLineLoc = self.size[0] - self.margins*2
+        """换行的位置"""
+
+    def init(self):
+        self.vision = self.frameList[0].copy()
+        self.currentWordLoc = [self.margins, self.margins]
+
+    def wrapText(self):
+        self.currentWordLoc[0] = self.margins
+        self.currentWordLoc[1] += self.textLineGap + self.textSize
+        #换行的判断及执行
+
+    def printer(self,msg, color = None):
+        if color == None:
+            self.color = self.textColor
+        else:
+            self.color = color
+        #设置颜色
+
+        self.msgImg = self.font.render(msg,False,self.color)
+        #生成文字图像
+        
+        self.vision.blit(self.msgImg,self.currentWordLoc)
+        #将文字图像绘上画板
+        
+        self.currentWordLoc[0] += self.msgImg.get_size()[0]
+        #决定下一个字符位置
+        if self.currentWordLoc[0] >= self.textLineCutLineLoc:
+            self.wrapText()
+
+
+class LogsBox:
+    """在每次刷新textBox的vision之前，将其copy一份扔到textVisionList里"""
+    def __init__(self,loc = [0,0], size = [10,10], textBoxHeight = 0 ):
+        self.activeSituation = False
+        self.animateSym = False
+        self.loc = loc
+        self.size = size
+        self.rect = loc+size
+        self.vision = pygame.Surface(size)
+        self.tempVision = pygame.Surface(size)
+
+        self.backGroundVision = pygame.Surface(size)
+        self.textVisionList = []
+        """新的语句应该插入到队列头部"""
+
+        self.textBoxHeight = textBoxHeight
+        #每个文本的高度
+        self.slideY = 0
+        """#记录滚轮滑动的距离，因为该功能只许玩家向上看之前的记录，故，滚轮向上该值增大，滚轮向下该值减小，
+        并且不允许小于0,不允许大于 (len(self.textVisionList)-1) * self.textBoxHeight """
+    def init(self):
+        self.activeSituation = False
+        self.slideY = 0
+
+    def animate(self):
+        """每次滚动滑轮时调用该方法,更新画面用"""
+        #画上背景
+        if self.animateSym == True:
+            if self.slideY > (len(self.textVisionList)-1) * self.textBoxHeight:
+                self.slideY -= 10
+            elif self.slideY < 0:
+                self.slideY += 10
+            else:
+                self.animateSym = False
+
+            self.tempVision.blit(self.backGroundVision,self.loc)
+            
+            index = 0
+            for textVision in self.textVisionList:
+                index += 1
+                currentTextLoc  = self.size[1] - self.textBoxHeight*index + self.slideY
+                if currentTextLoc > self.size[1]:
+                    continue
+
+                elif currentTextLoc > -self.textBoxHeight :
+                    self.tempVision.blit(textVision,[0,currentTextLoc])
+
+                elif currentTextLoc < -self.textBoxHeight :
+                    break
+            self.vision = self.tempVision.copy()
+
+
+class Character:
+    """你需要先创建表情Expression，并添加进expressionList里"""
+    def __init__(self, loc = [0,0], size = [0,0], color = (255,255,255)):
+        self.activeSituation = False
+
+        self.onStage = False
+
+        self.loc = loc
+        self.size = size
+        
+        self.color = color
+
+        self.expressionList = []
+        self.currentExpression = None
+        #表情列表与当前的表情
+
+    
+    def init(self):
+        self.activeSituation = False
+        for expression in self.expressionList:
+            expression.init()
+
+    def setExpression(self, expression):
+        self.currentExpression = self.expressionList[expression]
+        
+    def draw(self):
+        GE.camera.draw(self.currentExpression.vision,self.loc)
+        GE.camera.draw(self.currentExpression.mouth.vision,self.returnPartsAbsLoc(self.currentExpression.mouth.loc))
+        GE.camera.draw(self.currentExpression.eyes.vision,self.returnPartsAbsLoc(self.currentExpression.eyes.loc))
+        pass
+    def returnPartsAbsLoc(self,partsLoc):
+        return (self.loc[0]+partsLoc[0],self.loc[1]+partsLoc[1])
+
+
+    def animate(self):
+        #可能一个是否眨眼的判断
+        #self.vision = self.frameList[0].copy()
+        self.currentExpression.eyes.animate()
+        if self.activeSituation:
+            self.currentExpression.mouthAct()
+
+class Expression:
+    """你需要给body给予一张图片，并赋予eyes与mouth对象"""
+    def __init__(self):
+        self.vision = None
+        self.mouth = None
+        self.eyes = None
+        self.body = None
+
+    def init(self):
+        self.vision = self.body.copy()
+
+        self.mouth.vision = self.mouth.frameList[0]
+
+        self.eyes.vision = self.eyes.frameList[0]
+    
+    def mouthAct(self):
+        self.mouth.animate()
+
+    def animate(self):
+        #可能一个是否眨眼的判断
+        #self.vision = self.frameList[0].copy()
+        self.eyes.animate()
+
+
+class Eye:
+    def __init__(self, loc = [0,0], size = [10,10], eyesBlinkGap = 3):
+        self.loc = loc
+
+        self.currentEye = 0
+
+        self.BlinkGap = eyesBlinkGap * setting.animateLoopFps
+        self.BlinkGapTimer = 0
+        self.blinkTimer = 0
+
+        self.vision = pygame.Surface(size)
+        self.frameList = [] 
+        self.timeStampList = [40,41,44,46]
+        self.frameList = [0,1,2,1]
+    def init(self):
+        self.blinkTimer = 0
+        self.currentFrame = 0
+        self.vision = self.frameList[self.frameList[0]]
+        self.activeSituation = False
+        pass
+    
+    def animate(self):
+        self.eyesBlinkTimer += 1
+
+        if self.eyesBlinkTimer >= self.BlinkGap:
+            self.eyesBlinkTimer = 0
+            self.blink()
+
+    def blink(self):
+        self.blinkTimer += 1
+        #timer每动画帧自加一
+        if self.blinkTimer >= self.timeStampList[-1]:
+            self.init()
+        #如果timer等于动画周期，重置自身
+        if self.blinkTimer >= self.timeStampList[self.currentFrame]:
+            self.currentFrame += 1
+            self.vision = self.frameList[self.frameList[self.currentFrame]]
+        #如果timer等于当前帧对应的时间戳，则将当前帧记号加一,更新obj的vision
+
+class Mouth:
+    def __init__(self, loc = [0,0]):
+        self.loc = loc
+
+        self.currentMouth = 0
+
+        self.mouthActTimer = 0
+
+        self.vision = pygame.Surface((10,10))
+        self.frameList = []        
+        pass
+
+    def init(self):
+        self.currentMouth = 0
+        self.vision = self.frameList[0]
+
+    def animate(self):
+        self.vision = self.frameList[self.currentMouth]
+        self.currentMouth += random.randint(1,2)
+
+        if self.currentMouth >= len(self.frameList):
+            self.currentMouth = 1
--- data/setting.py
+++ data/setting.py
@@ -9,10 +9,23 @@ windowflags = (pygame.RESIZABLE|pygame.DOUBLEBUF)#可调节窗口大小的模式
 
 START = "开始";LOAD = "载入";OPTION = "设置";QUIT = "退出"
 
-charType = "resources/Font/bb4171.ttf"
 
-skip = 'left ctrl'
-autoPlay = "a"
+
+skip = 'left ctrl'; autoPlay = "a"
 
 printSpeed = 0.5
+
+textLineGap = 18
+
+textColor = (255,255,255)
+
+textSize = 30
+
+charBlodType = "source/font/bb4171.ttf"
+
+charType = "resources/Font/bb4171.ttf"
+
+textBlodSize = 0
+
+margins = 60
 
